c542bcbf330a69c44e0ed8079f1f0c60fe42561c
Victor Ndiritu <87909835+VickyTaar@users.noreply.github.com>
2024-01-12 15:21:11 +0300

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm an Information Security and Forensics student studying at KCA University. I enjoy building solutions to create a secure environment.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Cyber Security",
+  "Cloud Engineering",
+  "DevSecOps",
+  "Digital Forensics",
+  "Security Automation",
+  "DevOps",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my  Security and Cloud experience , I continually look for new and better ways to make tech secure accessible and by all.";
 
 const About = () => {
   return (
